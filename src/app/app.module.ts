f7353d38b76635fcc65451e7eea20e5779b29bb7
Subhash Rawat <subhash@techmatys.com>
2022-02-16 20:59:06 +0530

    initial folder structure changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './admin/pages/employee/employee.component';
+import { ProjectComponent } from './admin/pages/project/project.component';
+import { EmployeeProjectComponent } from './admin/pages/employee-project/employee-project.component';
+import { UserComponent } from './admin/pages/user/user.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { LoginComponent } from './home/login/login.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    ProjectComponent,
+    EmployeeProjectComponent,
+    UserComponent,
+    FooterComponent,
+    HeaderComponent,
+    SideNavComponent,
+    LoginComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

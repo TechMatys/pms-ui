48dccbe6afad970895923aa33653b4afd8b7790b
Subhash Rawat <subhash@techmatys.com>
2022-02-17 19:46:38 +0530

    Page route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,27 @@ import { ProjectComponent } from './admin/pages/project/project.component';
 import { EmployeeComponent } from './admin/pages/employee/employee.component';
 import { EmployeeProjectComponent } from './admin/pages/employee-project/employee-project.component';
 import { UserComponent } from './admin/pages/user/user.component';
+import { EmployeePaymentComponent } from './admin/pages/employee-payment/employee-payment.component';
+import { ProjectPaymentComponent } from './admin/pages/project-payment/project-payment.component';
 
 const routes: Routes = [
   {
-    path:'', component: DashboardComponent
+    path: '', component: DashboardComponent
   },
   {
-    path:'Project', component: ProjectComponent
+    path: 'Project', component: ProjectComponent
   },
   {
-    path:'Employee', component: EmployeeComponent
+    path: 'ProjectPayment', component: ProjectPaymentComponent
+  },
+  {
+    path: 'Employee', component: EmployeeComponent
   },
   {
     path: 'EmployeeProject', component: EmployeeProjectComponent
+  },
+  {
+    path: 'EmployeePayment', component: EmployeePaymentComponent
   },
   {
     path: 'User', component: UserComponent

8d0f45e519a6720ea518a46bd2635849832a016d
Subhash Rawat <subhash@techmatys.com>
2022-02-17 19:01:56 +0530

    added project and employee payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { LoginComponent } from './home/login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProjectPaymentComponent } from './project-payment/project-payment.component';
+import { EmployeePaymentComponent } from './employee-payment/employee-payment.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     SideNavComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    ProjectPaymentComponent,
+    EmployeePaymentComponent
   ],
   imports: [
     BrowserModule,

8995b6775c0e8132b621361f9f8092787fe14756
vikasrwt <86585848+vikasrwt@users.noreply.github.com>
2022-02-17 16:19:40 +0530

    added grid and form

--- src/app/admin/pages/project/project.component.ts
+++ src/app/admin/pages/project/project.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faEdit, faTrash, faEye
+ } from '@fortawesome/free-solid-svg-icons';
+
+
+interface Project
+ {
+  
+  name: string;
+  starting: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-project',
@@ -6,10 +18,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
+  project= new FormGroup ({
+    Name: new FormControl(''),
+    starting: new FormControl(''),
+    status: new FormControl(''),
+   
+    });
+  
+ faEdit=faEdit
+ faDelete= faTrash
+  projectlist: Project []
+  
+  = [
+    {
+      name: 'Vikas Rawat',
+     starting : '10-05-2020',
+      status: 'In Progress',
+     
+    },
+    {
+      name: 'Rudra Pratap Singh Bartwal',
+     starting : '19-05-2021',
+      status: 'In Progress',
+     
+    },
+    {
+      name: 'Prakash Rawat',
+     starting : '10-04-2020',
+      status: 'In Progress',
+     
+    },
+    {
+      name: 'Sumit Rawat',
+     starting : '20-04- 2020',
+      status: 'In Progress',
+     
+    },
+    {
+      name: 'Ganesh Rawat',
+     starting : '10-06-2020',
+     status: 'In Progress',
+     
+    },
+  ]
+
+  isShown: boolean = false;
+  isAddNew: boolean = true;
+  projectList: any;
+  createProjectForm: any;
+  projectData: any;
+  projectForm: any;
+
+  
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  // Function to add new button
+addbutton(){
+  this.isShown = false;
+  this.isAddNew = true;
+  this.projectList = [];
+  const data = {
+    projectName: '',
+    starting: '',
+    status: ''
+  }
+  
+  
+    }
+  
+    save() {
+      this.projectData = this.projectForm.value;
+  
+      const data = Object.assign({}, this.projectData);
+    }
+    
+
+  ngOnInit(): void {{this.isShown = !this.isShown;
+  }
   }
 
 }

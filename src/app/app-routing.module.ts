ebf1d431dad2022d8643d1217c428eee32f6881f
Subhash Rawat <subhash@techmatys.com>
2022-02-17 08:40:10 +0530

    add css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
+import { ProjectComponent } from './admin/pages/project/project.component';
+import { EmployeeComponent } from './admin/pages/employee/employee.component';
+import { EmployeeProjectComponent } from './admin/pages/employee-project/employee-project.component';
+import { UserComponent } from './admin/pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: DashboardComponent
+  },
+  {
+    path:'Project', component: ProjectComponent
+  },
+  {
+    path:'Employee', component: EmployeeComponent
+  },
+  {
+    path: 'EmployeeProject', component: EmployeeProjectComponent
+  },
+  {
+    path: 'User', component: UserComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

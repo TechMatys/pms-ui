65799b40efbb80abbdfdf718708aa8ea8e4cfa9e
PRAKASHRAWAT33 <86583004+PRAKASHRAWAT33@users.noreply.github.com>
2022-02-18 18:39:48 +0530

    employee payment table works

--- src/app/admin/pages/employee-payment/employee-payment.component.ts
+++ src/app/admin/pages/employee-payment/employee-payment.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+interface Employer {
+  employeename: string,
+  amount: string;
+  month: string;
+  paymentdate: string;
+}
 
 @Component({
   selector: 'app-employee-payment',
@@ -7,7 +15,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeePaymentComponent implements OnInit {
 
-  constructor() { }
+  faEdit = faEdit;
+  faDelete = faTrash;
+  isShown: boolean = true;
+  isAddNew: boolean = true;
+  selectedYear: number;
+  years: number[] = [];
+  selectedMonth: number;
+  months: number[] = [];
+
+  employer: Employer[] = [
+    {
+      employeename: 'Subhash Rawat',
+      amount: '70000',
+      month: '02/2022',
+      paymentdate: '28/2/2022',
+
+    },
+    {
+      employeename: 'Tajwar Rawat',
+      amount: '90000',
+      month: '02/2022',
+      paymentdate: '28/2/2022',
+
+    },
+    {
+      employeename: 'Prakash Rawat',
+      amount: '50000',
+      month: '02/2022',
+      paymentdate: '28/2/2022',
+
+    },
+    {
+      employeename: 'Vikash Rawat',
+      amount: '40000',
+      month: '02/2022',
+      paymentdate: '28/2/2022',
+
+    },
+
+    {
+      employeename: 'Deepak Dhiman',
+      amount: '100000',
+      month: '02/2022',
+      paymentdate: '28/2/2022',
+
+    },
+  ]
+
+
+
+  constructor() {
+    this.selectedYear = new Date().getFullYear();
+    for (let year = this.selectedYear; year >= 2020; year--) {
+      this.years.push(year);
+    }
+    this.selectedMonth = new Date().getMonth() + 11;
+    for (let month = this.selectedMonth; month >= 1; month--) {
+      this.months.push(month);
+    }
+  }
+
+  addEmployer() {
+    this.isShown = false;
+    this.isAddNew = true;
+  }
 
   ngOnInit(): void {
   }

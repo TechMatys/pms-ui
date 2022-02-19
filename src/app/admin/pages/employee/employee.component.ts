594aef63c2edb03fed4f8e174ac535048f93799f
PRAKASHRAWAT33 <86583004+PRAKASHRAWAT33@users.noreply.github.com>
2022-02-19 15:31:43 +0530

    employee table changing

--- src/app/admin/pages/employee/employee.component.ts
+++ src/app/admin/pages/employee/employee.component.ts
@@ -4,6 +4,7 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 interface Employer {
   name: string,
+  emailId: string;
   startDate: string;
   designation: string;
 }
@@ -23,21 +24,25 @@ export class EmployeeComponent implements OnInit {
   employer: Employer[] = [
     {
       name: 'Earl of Lemongrab',
+      emailId: 'earllemongrab12@gmail.com',
       startDate: '12/11/2000',
       designation: 'Lemon Candy',
     },
     {
       name: 'Bonnibel Bubblegum',
+      emailId: 'bonnibelbubblegum13@gmail.com',
       startDate: '14/05/1990',
       designation: 'Lemon Candy',
     },
     {
       name: 'Phoebe',
+      emailId: 'phoebe15@gmail.com',
       startDate: '17/12/2010',
       designation: 'Lemon Candy',
     },
     {
       name: 'Lumpy Space Princess',
+      emailId: 'lumpyprincess20@gmail.com',
       startDate: '14/07/2005',
       designation: 'Lemon Candy',
     },
@@ -57,7 +62,38 @@ export class EmployeeComponent implements OnInit {
     id: 1, name: 'Developer'
   }, {
     id: 2, name: 'Manager'
-  }];
+  }];  
+
+  // city name create
+  citys = [{
+    id: 0, name: '-- Select Citys --'
+  }, {
+    id: 1, name: 'Rudraprayag'
+  }, {
+    id: 2, name: 'Dehradun'
+  }, {
+     id: 3, name: 'Pauri Garhwal'
+},  {
+     id: 4, name: 'Gaya'
+}, {
+    id: 5, name: ' Darbhanga'
+}, {
+    id: 6, name: ' Dharamshala'
+},{
+     id: 7, name: ' Solan'
+},];
+
+// State name create
+
+states = [{
+  id: 0, name: '-- Select State --'
+}, {
+  id: 1, name: 'Uttarakhand'
+}, {
+  id: 2, name: 'Bihar'
+}, {
+  id: 2, name: 'Himachal Pradesh'
+}];
 
   constructor() {
   }

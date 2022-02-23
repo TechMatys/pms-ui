93b04954cc6f2ab10aba399864b29afcab362a8c
vikasrwt <86585848+vikasrwt@users.noreply.github.com>
2022-02-23 15:16:51 +0530

    minor changes in bar chart on dashboard

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,32 +15,49 @@ export class DashboardComponent implements OnInit {
 
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
+    // We use these empty structures as placeholders for dynamic theming.scaleLabel: "<%= ' $' + Number(value)%>"
     scales: {
-      x: {},
+      x: { max:5,
+        
+        grid: {
+        display: false
+        
+      }},
       y: {
-        min: 10
-      }
-    },
+        max: 250000,
+        grid: {
+          display: false
+        }
+        
+      } },
+      
     plugins: {
       legend: {
         display: true,
       },
       datalabels: {
         anchor: 'end',
-        align: 'end'
-      }
-    }
+        align: 'end',
+        display : false
+      } 
+    } 
   };
+ 
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [
-    DataLabelsPlugin
+    DataLabelsPlugin 
   ];
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '2010','2011','2012','2013','2014','2015','2016', '2017', '2018', '2019', '2020', '2021', '2022' ],
+    labels: [ 'January','February','March','April','May','June','July', 'August', 'September', 'October', 'November', 'December' ],
     datasets: [
-      { data: [ 90, 40, 20, 50, 30, 40 ], label: 'REVENUE' }
+      {  backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+      maxBarThickness: 30,
+      
+      data: [  90000, 140000, 20000, 150000, 30000, 120000 ], label: "Revenue "   }
+     
     ]
   };
 
@@ -64,11 +82,17 @@ export class DashboardComponent implements OnInit {
       40 ];
 
     this.chart?.update();
+     {
+      
+    }
+   
+    
   }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }

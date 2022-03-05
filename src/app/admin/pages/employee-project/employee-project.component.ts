0f95d1012c6eaf1ea7af0474dd9a13c90c4e8223
vikasrwt <86585848+vikasrwt@users.noreply.github.com>
2022-03-05 17:44:16 +0530

    PMS-107_Employee-project-Crud-Operation:- Added crud operation in employee project

--- src/app/admin/pages/employee-project/employee-project.component.ts
+++ src/app/admin/pages/employee-project/employee-project.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { HttpService } from 'src/app/core/services/https/http.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PopUpService } from 'src/app/core/services/pop-up/pop-up.service';
+
 
 interface EmployeeProject {
   name: string;
   project: any;
   assignedDate: any;
+}
+
+interface Employee {
+  employeeId: number;
+  employeeName: string;
+}
+
+interface Project {
+ projectId:number;
+ projectName: string;
 }
 
 @Component({
@@ -20,77 +35,96 @@ export class EmployeeProjectComponent implements OnInit {
   faDelete = faTrash;
   isShown: boolean = true;
   isAddNew: boolean = true;
-  employeeProjectList: any;
+  
+  controllerName = "Employee-Project";
 
-  employeeProjectlist: EmployeeProject[] = [
-    {
-      name: 'Tajwar Rawat',
-      project: 'Project 1',
-      assignedDate: '10/05/2020',
-    }, {
-      name: 'Prakash Rawat',
-      project: 'Project 2',
-      assignedDate: '10/05/2020',
-    }, {
-      name: 'Vikas Rawat',
-      project: 'Project 3',
-      assignedDate: '10/05/2020',
-    }, {
-      name: 'Deepak Dhiman',
-      project: 'Project 4',
-      assignedDate: '10/05/2020'
-    }];
-
-  employees = [
-    {
-      id: 0, name: '-- Select Name--'
-    }, {
-      id: 1,
-      name: 'Anoop Rawat'
-    }, {
-      id: 2,
-      name: 'Nirmla Rawat'
-    }, {
-      id: 3,
-      name: 'Anjali Rawat'
-    }, {
-      id: 4,
-      name: 'Deepika Rawat'
-    }];
-
-  projects = [
-    {
-      id: 0, name: '-- Select Project--'
-    }, {
-      id: 1,
-      name: 'Project Breeze'
-    }, {
-      id: 2,
-      name: 'Dynamic Program'
-    }, {
-      id: 3,
-      name: 'Magnetic Program'
-    }, {
-      id: 4,
-      name: 'Project Signal'
-    }];
+  employeeProjectList: EmployeeProject[] = [];
+  employeesList: Employee[] = [];
+  projects: Project[] = [];
+  employeeProjectForm: FormGroup;
+  submitted = false;
   today: Date;
 
-  constructor() { this.today = new Date();}
+  constructor(private http: HttpService, private formBuilder: FormBuilder, private toastr: ToastrService, private popUpService: PopUpService,) { this.today = new Date();
+   
+    this.employeeProjectForm = this.formBuilder.group({
+      employeeId: [0],
+      projectId: [0],
+      assignedDate: new FormControl(this.today),
+      notes: [null],
+      managedBy: [-1]
+    });}
 
+  
+
+  
   // Function to add new button
   addEmployeeProject() {
     this.isShown = false;
     this.isAddNew = true;
     this.employeeProjectList = [];
   }
+  getAllEmployees() {
+    this.http.getAll('Employee').subscribe(res => {
+      this.employeeProjectList = res;
+    });
+  }
+
+  deleteEmployee(employeeProject: any) {
+    this.popUpService.confirm('Confirmation', 'Are you sure you want to delete this employee project?', 'Yes', 'No', 'md')
+      .then((confirmed) => {
+        if (confirmed) {
+          this.http.delete(this.controllerName, employeeProject.employeeProjectId)
+            .subscribe(res => {
+              this.toastr.success("Employee project deleted successfully", "Success");
+              this.getAllEmployeeProject();
+            });
+        }
+      });
+      
+  }
+
+  editEmployee(employeProject: any) {
+    this.http.get(this.controllerName, employeProject.employeeProjectId)
+      .subscribe(res => {
+        this.isShown = false;
+        this.employeeProjectForm.setValue(res);
+      });
+  }
+  saveEmployeeProject() {
+    this.submitted = true;
+
+    
+    this.employeeProjectForm.controls['managedBy'].setValue(-1);
+
+    const employeeProjectData = this.employeeProjectForm.value;
+    const employeeProjectId = employeeProjectData.employeeProjectId;
+
+    if (employeeProjectId < 1) {
+      this.http.create(this.controllerName, employeeProjectData)
+        .subscribe(res => {
+          this.toastr.success("Employee project added successfully", "Success");
+          this.getAllEmployeeProject();
+        });
+    }
+    else {
+      this.http.update(this.controllerName, employeeProjectId, employeeProjectData)
+        .subscribe(res => {
+          this.toastr.success("Employee project updated successfully", "Success");
+          this.getAllEmployeeProject();
+        });
+    }
+  }
+  getAllEmployeeProject() {
+    this.isShown = true;
+    this.http.getAll(this.controllerName).subscribe(res => {
+      this.employeeProjectList = res;
+    });
+  }
 
   ngOnInit(): void {
+    this.getAllEmployees();
+    this.getAllEmployeeProject();
   }
-
-  onProjectChange(item: any) {
-  }
-
-  onEmployeeChange(item: any) {
-  }
+ 
 }

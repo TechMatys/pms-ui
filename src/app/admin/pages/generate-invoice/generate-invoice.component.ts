5260997c68da8027fa8012cf5ece5c4d72bd7d2f
PRAKASHRAWAT33 <86583004+PRAKASHRAWAT33@users.noreply.github.com>
2022-03-10 17:26:54 +0530

    new formcontrol in grnerateinvoice

--- src/app/admin/pages/generate-invoice/generate-invoice.component.ts
+++ src/app/admin/pages/generate-invoice/generate-invoice.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { faEdit, faTrash, faEye, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { HttpService } from 'src/app/core/services/https/http.service';
+import { PopUpService } from 'src/app/core/services/pop-up/pop-up.service';
 
 interface GenerateInvoice {
   title: string,
@@ -12,43 +16,112 @@ interface GenerateInvoice {
   styleUrls: ['./generate-invoice.component.scss']
 })
 export class GenerateInvoiceComponent implements OnInit {
+
+  invoiceForm: FormGroup;
+  submitted = false;
+
   faDownload = faDownload;
   faEye = faEye;
   faEdit = faEdit;
   faDelete = faTrash;
   isShown: boolean = true;
   isAddNew: boolean = true;
+  
+  controllerName = "Company-Invoice";
 
-  dtOptions: DataTables.Settings = {};
+  // dtOptions: DataTables.Settings = {};
 
-  generateInvoiceList: GenerateInvoice[] = [
-    {
-      title: 'Invoice1',
-      createdBy: 'Admin',
-      generatedDate: '28/02/2022'
-    }, {
-      title: 'Invoice2',
-      createdBy: 'Admin',
-      generatedDate: '28/02/2022'
-    }, {
-      title: 'Invoice3',
-      createdBy: 'Staff',
-      generatedDate: '28/02/2022'
-    }, {
-      title: 'Invoice4',
-      createdBy: 'Staff',
-      generatedDate: '28/02/2022'
-    }];
+  generateInvoiceList: GenerateInvoice[] = [];
+  today: Date;
 
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService, private popUpService: PopUpService,
+    private http: HttpService) {
+
+      this.today = new Date();
+      this.invoiceForm = this.formBuilder.group({
+        companyInvoiceId: [0],
+        title: [null, Validators.required],
+        createdBy: [null],
+        generatedDate: new FormControl(this.today),
+        managedBy: [-1]
+      });
+    }
+  
+     
   generateInvoice() {
+    this.resetForm();
     this.isShown = false;
     this.isAddNew = true;
   }
 
+  resetForm() {
+    this.submitted = false;
+    this.invoiceForm.reset();
+    this.invoiceForm.controls['generatedDate'].setValue(this.today);
+    this.invoiceForm.controls['companyInvoiceId'].setValue(0);
+    this.invoiceForm.controls['managedBy'].setValue(-1);
+  }
+
+  deleteGenerateInvoice(generateInvoice: any) {
+    this.popUpService.confirm('Confirmation', 'Are you sure you want to delete this GenerateInvoice?', 'Yes', 'No', 'md')
+      .then((confirmed) => {
+        if (confirmed) {
+          this.http.delete(this.controllerName, generateInvoice.companyInvoiceId)
+            .subscribe(res => {
+              this.toastr.success("GenerateInvoice deleted successfully", "Success");
+              this.getAllGenerateInvoiceList();
+            });
+        }
+      });
+  }
+
+  get f() { return this.invoiceForm.controls; }
+  
+
+  saveGenerateInvoice() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.invoiceForm.invalid) {
+      return;
+    }
+
+
+
+    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.invoiceForm.value))
+
+    this.invoiceForm.controls['managedBy'].setValue(-1);
+
+    const generateInvoiceData = this.invoiceForm.value;
+    const companyInvoiceId = generateInvoiceData.companyInvoiceId;
+    if (companyInvoiceId < 1) {
+      this.http.create(this.controllerName, generateInvoiceData)
+        .subscribe(res => {
+          this.toastr.success("GenerateInvoice created successfully", "Success");
+          this.getAllGenerateInvoiceList();
+        });
+    }
+    else {
+      this.http.update(this.controllerName, companyInvoiceId, generateInvoiceData)
+        .subscribe(res => {
+          this.toastr.success("GenerateInvoice updated successfully", "Success");
+          this.getAllGenerateInvoiceList();
+        });
+    }
+  }
+
+  getAllGenerateInvoiceList() {
+    this.isShown = true;
+    this.http.getAll(this.controllerName).subscribe(res => {
+      this.generateInvoiceList = res;
+    });
+
+  }
+
 
   ngOnInit(): void {
+    this.getAllGenerateInvoiceList();
   }
 
 }

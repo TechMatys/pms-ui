e8a0338677834e7dc528b900ed17880c258d5377
Subhash Rawat <subhash@techmatys.com>
2022-03-10 19:38:14 +0530

    new component added

---
+++ src/app/admin/pages/company-invoice/company-invoice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-invoice',
+  templateUrl: './company-invoice.component.html',
+  styleUrls: ['./company-invoice.component.scss']
+})
+export class CompanyInvoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

56d60450e7f22a733357cdcb679f090eb1aee619
Subhash Rawat <subhash@techmatys.com>
2022-10-22 12:43:08 +0530

    url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7051/',
+  apiUrl: 'http://192.168.1.46:8081/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7051/'
+  apiUrl: 'http://192.168.1.46:8081/'
 };
 
 /*

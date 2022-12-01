9f305a778136826639103ef983f9314f26fc24d9
Subhash Rawat <subhash@techmatys.com>
2022-12-01 09:34:11 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://localhost:7051/',
+  // apiUrl: 'http://192.168.29.176:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7051/'
+  apiUrl: 'https://localhost:7051/',
+  // apiUrl: 'http://192.168.29.176:8080/'
 };
 
 /*
